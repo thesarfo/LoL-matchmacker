a3dc545758e4745ce28d0734c195b00d4120886e
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-22 09:01:09 +0000

    feat: update build id, and register players

--- bot/db_utils.py
+++ bot/db_utils.py
@@ -10,3 +10,21 @@ def load_json() -> dict[str, dict | float | str]:
         json = load(file)
         return json
 
+def update_build_id(build_id: str):
+    json: dict[str, dict | float | str] = load_json()
+    json["last_refresh"] = default_timer()
+    json["build_id"] = build_id
+    with open(normpath(f"{data_folder}/db.json"), "w") as file:
+        dump(json, file, indent=4)
+
+
+def register_player(summoner: str, discord_id: str):
+    json: dict[str, dict] = load_json()
+
+    if discord_id in json["players"]:
+        json["players"][discord_id]["summoner"] = summoner
+    else:
+        json["players"][discord_id] = {"summoner": summoner}
+
+    with open(normpath(f"{data_folder}/db.json"), "w") as file:
+        dump(json, file, indent=4)
