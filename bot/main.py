1a23ad21c230551528e5463bd2dd1817e02848ae
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-22 08:59:14 +0000

    add close command to finalize teams, initialize bot client and start the game if there's enough players

--- bot/main.py
+++ bot/main.py
@@ -70,3 +70,55 @@ class MatchMaker(Client):
                     self.playing_list.remove(player.get("summoner"))
                     self.playing_list_ids.pop(player.get("summoner"))
                     await self.send_ready_list(message)
+            case ["!close"]:
+                if len(self.playing_list) < 10:
+                    await message.channel.send(
+                        "You don't have enough players to play, you need at least 10"
+                    )
+                else:
+                    output_string = "Ready to play: \n"
+                    for player in enumerate(self.playing_list, start=1):
+                        output_string += "{}. {}\n".format(*player)
+                    await message.channel.send(output_string)
+                    await message.channel.send(
+                        "Starting the draw! Give me some seconds."
+                    )
+                    blue_team, red_team, ranks = await balance(self.playing_list)
+                    await message.channel.send(
+                        beautify_teams(
+                            blue_team, red_team, ranks, self.guilds[0].emojis
+                        )
+                    )
+                    blue_team_channel = utils.get(
+                        self.guilds[0].channels, name="Team 1"
+                    )
+                    red_team_channel = utils.get(self.guilds[0].channels, name="Team 2")
+                    for player in blue_team:
+                        await (
+                            self.guilds[0]
+                            .get_member(self.playing_list_ids.get(player))
+                            .move_to(blue_team_channel)
+                        )
+                    for player in red_team:
+                        await (
+                            self.guilds[0]
+                            .get_member(self.playing_list_ids.get(player))
+                            .move_to(red_team_channel)
+                        )
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    if not exists(data_folder):
+        mkdir(data_folder)
+    if not exists(normpath(f"{data_folder}/db.json")):
+        print("Data folder created")
+        with open(normpath(f"{data_folder}/db.json"), "w") as file:
+            dump(
+                {"players": {}, "last_refresh": None, "build_id": None}, file, indent=4
+            )
+    _intents = Intents.default()
+    _intents.message_content = True
+
+    client = MatchMaker(intents=_intents)
+    client.run(getenv("TOKEN"))
