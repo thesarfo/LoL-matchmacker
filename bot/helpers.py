dcde659bfb15aed043c518760afdd6ceeba89dba
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-22 09:05:12 +0000

    function to format and display game teams with their scores and ranks

--- bot/helpers.py
+++ bot/helpers.py
@@ -11,34 +11,36 @@ from bot.db_utils import load_json, update_build_id
 from bot.scrapper import get_rank, get_build_id
 
 
-async def draw(mid_point: float, list_ranked_summoners: list[tuple[str, int]]) -> \
-        tuple[dict[str, int | list], dict[str, int | list]]:
-    blue_team = {'score': 0, 'players': []}
-    while blue_team['score'] < mid_point:
+async def draw(
+    mid_point: float, list_ranked_summoners: list[tuple[str, int]]
+) -> tuple[dict[str, int | list], dict[str, int | list]]:
+    blue_team = {"score": 0, "players": []}
+    while blue_team["score"] < mid_point:
         random_index = randint(0, len(list_ranked_summoners) - 1)
         random_summoner = list_ranked_summoners[random_index]
-        blue_team['players'].append(random_summoner[0])
-        blue_team['score'] += random_summoner[1]
+        blue_team["players"].append(random_summoner[0])
+        blue_team["score"] += random_summoner[1]
         a = list_ranked_summoners.pop(random_index)
-        if len(blue_team.get('players')) == 4:
-            comparison_value = mid_point - blue_team['score']
+        if len(blue_team.get("players")) == 4:
+            comparison_value = mid_point - blue_team["score"]
             elected_summoner = 0
             min_value = abs(list_ranked_summoners[0][1] - comparison_value)
             for j in range(0, len(list_ranked_summoners) - 1):
                 if abs(list_ranked_summoners[j][1] - comparison_value) < min_value:
                     elected_summoner = j
                     min_value = abs(list_ranked_summoners[j][1] - comparison_value)
-            blue_team['players'].append(list_ranked_summoners[elected_summoner][0])
-            blue_team['score'] += list_ranked_summoners[elected_summoner][1]
+            blue_team["players"].append(list_ranked_summoners[elected_summoner][0])
+            blue_team["score"] += list_ranked_summoners[elected_summoner][1]
             list_ranked_summoners.pop(elected_summoner)
             break
-        if len(blue_team['players']) == 5:
+        if len(blue_team["players"]) == 5:
             break
     red_team = {
-        'score': mid_point * 2 - blue_team.get('score'),
-        'players': [summoner[0] for summoner in list_ranked_summoners]
+        "score": mid_point * 2 - blue_team.get("score"),
+        "players": [summoner[0] for summoner in list_ranked_summoners],
     }
     return blue_team, red_team
+
 
 async def balance(
     summoners: list[str],
@@ -78,3 +80,55 @@ async def balance(
         if draws >= 1000:
             initial_threshold += 1
     return blue_team, red_team, mapped_results
+
+
+def beautify_teams(
+    blue_team: dict[str, int | list],
+    red_team: dict[str, int | list],
+    ranks: dict[str, str],
+    emojis=None,
+):
+    def helper_formatting(out, player=None, _emojis=None):
+        try:
+            if emojis:
+                out += "{}. {} {}\n".format(
+                    player[0],
+                    player[1],
+                    (
+                        utils.get(
+                            emojis, name=ranks.get(player[1]).split()[0].capitalize()
+                        )
+                    ),
+                )
+            else:
+                out += "{}. {} {}\n".format(
+                    player[0], player[1], (ranks.get(player[1]).split()[0])
+                )
+        except (Exception,):
+            return (
+                f"{player[0]}. {player[1]} -> There was a problem processing the rank"
+            )
+        return out
+
+    output_string = "Game: Closed\n"
+    output_string += "```ini\n" "[Blue Team with Score: {0:.2f}]\n" "```".format(
+        blue_team["score"]
+    )
+    x_blue_team = enumerate(blue_team["players"], start=1)
+    x_red_team = enumerate(red_team["players"], start=6)
+    for p in x_blue_team:
+        output_string = helper_formatting(output_string, player=p, _emojis=emojis)
+
+    output_string += (
+        "```ansi\n" "[2;31m[Red Team with Score: {0:.2f}][0m[2;31m[0m\n" "```".format(
+            red_team["score"]
+        )
+    )
+
+    for p in x_red_team:
+        output_string = helper_formatting(output_string, player=p, _emojis=emojis)
+
+    output_string += "```css\n" "[Average Score: {0:.2f}]\n" "```".format(
+        (red_team["score"] + blue_team["score"]) / 2
+    )
+    return output_string
